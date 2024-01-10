7cac0b355ab08a8844af1e8f6803c3a30ec394bd
manuelFrncisco <a21311092@uthermosillo.edu.mx>
2024-01-10 16:22:57 -0600

    Siguiendo y agregando carpetas

--- api/node/src/index.js
+++ api/node/src/index.js
@@ -1,10 +1,10 @@
 import { http } from 'node:http';
 
-const server = http.createServer((req, res) =>{
+const server = http.createServer((req, res) => {
     console.log('request received')
     res.end('Hola')
 })
 
-server.listen(0, () =>{
-    console.log(`servidor listening on port http://localhost:${server.address().port}` )
+server.listen(0, () => {
+    console.log(`servidor listening on port http://localhost:${server.address().port}`)
 })
