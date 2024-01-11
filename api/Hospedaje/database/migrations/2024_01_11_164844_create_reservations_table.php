585b200f4a86ae303437fcc50ae715cd06c266ec
manuelFrncisco <a21311092@uthermosillo.edu.mx>
2024-01-11 10:54:27 -0600

    Se creo todas las entidades para la base de datos

---
+++ api/Hospedaje/database/migrations/2024_01_11_164844_create_reservations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->integer("user_id");
+            $table->integer("location_id");
+            $table->integer("lodging_id");
+            $table->integer("rating_id");
+            $table->date("date");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
