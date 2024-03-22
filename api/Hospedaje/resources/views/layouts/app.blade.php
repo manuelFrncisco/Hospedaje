31ef33754a63250d444125ca9d2967589796bcb6
manuelFrncisco <a21311092@uthermosillo.edu.mx>
2024-03-22 12:19:11 -0600

    Se cambio la config.vite entonces me falto jiji

--- api/Hospedaje/resources/views/layouts/app.blade.php
+++ api/Hospedaje/resources/views/layouts/app.blade.php
@@ -15,7 +15,6 @@
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css">
     <link
         href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Oswald:wght@259&display=swap"
         rel="stylesheet">
@@ -23,9 +22,11 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
-    <link rel="stylesheet" href="{{asset('css/css.css')}}">
-    <link rel="stylesheet" href="{{asset('css.app.css')}}">
-    <script rel="stylesheet" href="{{asset('js/app.js')}}"></script>
+    @vite(['https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css'])
+    @vite(['resources/css/css.css'])
+
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
--- api/Hospedaje/resources/views/layouts/panel.blade.php
+++ api/Hospedaje/resources/views/layouts/panel.blade.php
@@ -10,7 +10,9 @@
     <title>@yield('title')</title>
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
     <link href="css/styles.css" rel="stylesheet" />
-    <link rel="stylesheet" href="{{asset('css.css.css')}}">
+
+    @vite(['https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css'])
+    @vite(['resources/css/css.css'])
 </head>
 
 <body class="sb-nav-fixed">
