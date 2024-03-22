3cf267a0fa69132bece256ed91203cf19b0a72bf
manuelFrncisco <a21311092@uthermosillo.edu.mx>
2024-03-22 13:09:34 -0600

    Error en el apartado

--- api/Hospedaje/resources/views/layouts/panel.blade.php
+++ api/Hospedaje/resources/views/layouts/panel.blade.php
@@ -10,7 +10,7 @@
     <title>@yield('title')</title>
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
     <link href="css/styles.css" rel="stylesheet" />
-    @vite(['resources/css/css.css'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="sb-nav-fixed">
