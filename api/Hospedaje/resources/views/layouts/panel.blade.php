eed9fc825fffae8f98a91d10d48f54a0b341c7e4
manuelFrncisco <a21311092@uthermosillo.edu.mx>
2024-03-22 12:24:34 -0600

    Se quito un vie jeje

--- api/Hospedaje/resources/views/layouts/panel.blade.php
+++ api/Hospedaje/resources/views/layouts/panel.blade.php
@@ -10,8 +10,6 @@
     <title>@yield('title')</title>
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
     <link href="css/styles.css" rel="stylesheet" />
-
-    @vite(['https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css'])
     @vite(['resources/css/css.css'])
 </head>
 
